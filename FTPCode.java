e9434436bc56f4cab7f311d3921bf1186c9fd7b1
aurelienbernard <aurelienpoolen@gmail.com>
2019-12-19 15:19:50 +0100

    code in front now

--- FTPCode.java
+++ FTPCode.java
@@ -1,96 +1,96 @@
 public class FTPCode {
 
 
-    String getMessage(int code){
+    String getMessage(Integer code){
         
         switch(code ) {
             case 125:
-                return "Data connection already open; transfer starting.\n\r";
+                return code.toString() + " Data connection already open; transfer starting.\n\r";
             case 150:
-                return "File status okay; about to open data connection.\n\r";
+                return code.toString() + " File status okay; about to open data connection.\n\r";
             case 200:
-                return "200 command successful\n\r";
+                return code.toString() + " Command successful\n\r";
             case 202:
-                return "Command not implemented, superfluous at this site.\n\r";
+                return code.toString() + " Command not implemented, superfluous at this site.\n\r";
             case 211:
-                return "System status, or system, help reply."; // needs precision
+                return code.toString() + " System status, or system, help reply."; // needs precision
             case 212:
-                return "Directory status.\n\r";
+                return code.toString() + " Directory status.\n\r";
             case 213:
-                return "File status.\n\r";
+                return code.toString() + " File status.\n\r";
             case 220: 
-                return "Service ready for new user. \n\r";
+                return code.toString() + " Service ready for new user. \n\r";
             case 221:
-                return "Service closing control connection.\n\r";
+                return code.toString() + " Service closing control connection.\n\r";
             case 225:
-                return "Data connection open; no transfer in progress.\n\r";
+                return code.toString() + " Data connection open; no transfer in progress.\n\r";
             case 226:
-                return "Closing data connection. Requested file action successful\n\r";
+                return code.toString() + " Closing data connection. Requested file action successful\n\r";
             case 227:
-                return "227 Entering Passive Mode\n\r";
+                return code.toString() + " Entering Passive Mode\n\r";
             case 228:
-                return "Entering Long Passive Mode\n\r";
+                return code.toString() + " Entering Long Passive Mode\n\r";
             case 229:
-                return "229 Entering Extended Passive Mode\n\r";
+                return code.toString() + " Entering Extended Passive Mode\n\r";
             case 230:
-                return "User logged in, proceed.\n\r";
+                return code.toString() + " User logged in, proceed.\n\r";
             case 231:
-                return "User logged out\n\r";
+                return code.toString() + " User logged out\n\r";
             case 232:
-                return "Logout command noted, will complete when transfer done.\n\r";
+                return code.toString() + " Logout command noted, will complete when transfer done.\n\r";
             case 250:
-                return "Requested file action okay, completed. \n\r";
+                return code.toString() + " Requested file action okay, completed. \n\r";
             case 253:
-                return "253 last modification data correctly retrieved \n\r";
+                return code.toString() + " last modification data correctly retrieved \n\r";
             case 257:
-                return " created.\n\r";
+                return code.toString() + " created.\n\r";
             case 331:
-                return "User name okay, need password.\n\r";
+                return code.toString() + " User name okay, need password.\n\r";
             case 332:
-                return "Need account for login.\n\r";
+                return code.toString() + " Need account for login.\n\r";
             case 350:
-                return "Requested file action pending further information \n\r";
+                return code.toString() + " Requested file action pending further information \n\r";
             case 421:
-                return "Service not available, closing control connection. This may be a reply to any command if the service knows it must shut down. \n\r";
+                return code.toString() + " Service not available, closing control connection. This may be a reply to any command if the service knows it must shut down. \n\r";
             case 425:
-                return "Can't open data connection.\n\r";
+                return code.toString() + " Can't open data connection.\n\r";
             case 426:
-                return "Connection closed; transfer aborted.\n\r";
+                return code.toString() + " Connection closed; transfer aborted.\n\r";
             case 430:
-                return "Invalid username or password \n\r";
+                return code.toString() + " Invalid username or password \n\r";
             case 434:
-                return "Requested host unavailable.\n\r";
+                return code.toString() + " Requested host unavailable.\n\r";
             case 450:
-                return "Requested file action not taken.\n\r";
+                return code.toString() + " Requested file action not taken.\n\r";
             case 451:
-                return "Requested action aborted. Local error in processing.\n\r";
+                return code.toString() + " Requested action aborted. Local error in processing.\n\r";
             case 452:
-                return "Requested action not taken.\n\r";
+                return code.toString() + " Requested action not taken.\n\r";
             case 500:
-                return "Syntax error in parameters or arguments.\n\r";
+                return code.toString() + " Syntax error in parameters or arguments.\n\r";
             case 502:
-                return "502 Command not implemented.\n\r";
+                return code.toString() + " Command not implemented.\n\r";
             case 503:
-                return "503 Bad sequence of commands.\n\r";
+                return code.toString() + " Bad sequence of commands.\n\r";
             case 504:
-                return "504 Command not implemented for those parameter.\n\r";
+                return code.toString() + " Command not implemented for those parameter.\n\r";
             case 530:
-                return "530 Not logged in.\n\r";
+                return code.toString() + " Not logged in.\n\r";
             case 550:
-                return "550 Requested action not taken. \n\r";
+                return code.toString() + " Requested action not taken. \n\r";
             case 551:
-                return"Requested action aborted. Page type unknown.\n\r";
+                return code.toString() + " Requested action aborted. Page type unknown.\n\r";
             case 552:
-                return"Requested file action aborted. Exceeded storage allocation\n\r";
+                return code.toString() + " Requested file action aborted. Exceeded storage allocation\n\r";
             case 553:
-                return "Requested action not taken. File name not allowed.\n\r";
+                return code.toString() + " Requested action not taken. File name not allowed.\n\r";
             case 631:
-                return "Integrity protected reply. \n\r";
+                return code.toString() + " Integrity protected reply. \n\r";
             case 10068:
-                return "Too many users, server is full.\n\r";
+                return code.toString() + " Too many users, server is full.\n\r";
 
         }
-        return "Error code not Found for this issue\n\r";
+        return code.toString() + " Error code not Found for this issue\n\r";
 
     }
 
